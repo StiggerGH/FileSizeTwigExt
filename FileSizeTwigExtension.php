759ca22e55553a419a855f437faa5969119f0a68
StiggerGH <vshamko@gmail.com>
2023-11-14 17:06:19 +0300

    Initial

---
+++ FileSizeTwigExtension.php
@@ -0,0 +1,24 @@
+<?
+
+namespace Spbitec;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class FileSizeTwigExtension extends AbstractExtension {
+   public function getFilters() {
+      return [
+         new TwigFilter('formatFileSize', [$this, 'formatFileSize']),
+      ];
+   }
+
+   public function formatFileSize($size) {
+      $units = ['B', 'KB', 'MB', 'GB', 'TB'];
+
+      for ($i = 0; $size >= 1024 && $i < 4; $i++) {
+         $size /= 1024;
+      }
+
+      return round($size, 2) . ' ' . $units[$i];
+   }
+}
